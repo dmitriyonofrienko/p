ee19e8cd71fb3a413b37970817bed1543bcc2328
Dmitriy <dmitriyonofrienko@gmail.com>
2025-02-11 01:53:23 +0400

    1

--- script.dev.js
+++ script.dev.js
@@ -1,15 +1,5 @@
 "use strict";
 
-//добавляет класс по скроллу
-//анимирует логотип в хедере
-// window.addEventListener("scroll", function () {
-//     const header = document.querySelector(".logo");
-//     if (window.scrollY > 5) {
-//       header.classList.add("scrolled");
-//     } else {
-//       header.classList.remove("scrolled");
-//     }
-//   });
 //fade up анимация для блоков с класом .fade-up
 var observer = new IntersectionObserver(function (entries) {
   entries.forEach(function (entry) {
@@ -25,22 +15,4 @@ var observer = new IntersectionObserver(function (entries) {
 });
 document.querySelectorAll(".fade-up").forEach(function (el) {
   return observer.observe(el);
-}); //   //плавный переход между страницами
-// document.addEventListener("DOMContentLoaded", () => {
-//   const page = document.querySelector("body");
-// // Делаем плавное появление после рендеринга
-// requestAnimationFrame(() => {
-//   page.classList.add("show");
-// });
-// // Плавное исчезновение при переходе
-// document.querySelectorAll("a").forEach(link => {
-//     link.addEventListener("click", (event) => {
-//       event.preventDefault();
-//       const href = link.getAttribute("href");
-//       // Скрываем страницу перед переходом
-//       page.classList.add("hide");
-//       setTimeout(() => {
-//         window.location.href = href;
-//       }, 150); // Время совпадает с transition в CSS
-//     });
-//   });
+});
--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-
-//добавляет класс по скроллу
-//анимирует логотип в хедере
-// window.addEventListener("scroll", function () {
-//     const header = document.querySelector(".logo");
-//     if (window.scrollY > 5) {
-//       header.classList.add("scrolled");
-//     } else {
-//       header.classList.remove("scrolled");
-//     }
-//   });
-
-
 //fade up анимация для блоков с класом .fade-up
 const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
@@ -23,29 +10,3 @@ const observer = new IntersectionObserver(entries => {
     });
   }, { threshold: 0.3 });
   document.querySelectorAll(".fade-up").forEach(el => observer.observe(el));
-
-
-
-//   //плавный переход между страницами
-// document.addEventListener("DOMContentLoaded", () => {
-//   const page = document.querySelector("body");
-
-// // Делаем плавное появление после рендеринга
-// requestAnimationFrame(() => {
-//   page.classList.add("show");
-// });
-
-// // Плавное исчезновение при переходе
-// document.querySelectorAll("a").forEach(link => {
-//     link.addEventListener("click", (event) => {
-//       event.preventDefault();
-//       const href = link.getAttribute("href");
-
-//       // Скрываем страницу перед переходом
-//       page.classList.add("hide");
-
-//       setTimeout(() => {
-//         window.location.href = href;
-//       }, 150); // Время совпадает с transition в CSS
-//     });
-//   });
